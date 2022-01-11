c91f0ad67dda6a00bd4edc7bb3e682b1527d9b9f
Dmitry Shirinkin <shirinkin1313@yandex.ru>
2022-01-11 11:44:30 +0300

    refactoring code

--- task_1.py
+++ task_1.py
@@ -15,9 +15,6 @@ def get_dishes_names(file_name):
     return dishes
 
 
-dishes = get_dishes_names('recipe')
-print(dishes)
-
 
 def get_ingredients(file_name):   
     ingredients_for_all_dishes = []    
@@ -28,10 +25,6 @@ def get_ingredients(file_name):
                 ingredients_for_all_dishes.append([file.readline().strip()])
 
     return ingredients_for_all_dishes
-
-
-ingredients_for_all_dishes = get_ingredients('recipe')
-print(ingredients_for_all_dishes)
 
 
 def split_on(list_for_split):
@@ -46,15 +39,11 @@ def split_on(list_for_split):
     return splitted
 
 
-result = split_on(ingredients_for_all_dishes)
-print(result)
-
-
 def cook_book_dict(file_name):
     cook_book_dictionary = {}
 
     for i in range(0, len(get_dishes_names(file_name))):
-        cook_book_dictionary[get_dishes_names(file_name)[i]] = result[i]
+        cook_book_dictionary[get_dishes_names(file_name)[i]] = split_on(get_ingredients(file_name))[i]
 
     return cook_book_dictionary
 
@@ -67,20 +56,17 @@ def upgrade_list(list_for_upgrade):  # приведем список к необ
             ing = str(ing)[2:-2]
             ing = ing.split('|')
         val.append(ing)
-        print(new_list)
+        #print(new_list)
 
     return list_for_upgrade
 
 
 
+if __name__ == '__main__':
 
+    print(split_on(get_ingredients('recipe')))
+    cook_book = cook_book_dict('recipe')
+    print(cook_book_dict('recipe'))
+    print(get_dishes_names('recipe'))
 
-cook_book = cook_book_dict('recipe')
-print(type(cook_book.values()))
-print(cook_book)
-#result = split_dict_values(cook_book_dictionary)
-print(result)
-print(type(result))
-result = upgrade_list(result)
-print(result)
 
